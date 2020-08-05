0eb82e88dc918f45bb43a332a57b11c916b8cbe9
Hopson97 <mhopson@hotmail.co.uk>
2020-08-05 14:34:11 +0100

    move the functions to new file

---
+++ evaluate.py
@@ -0,0 +1,76 @@
+'''
+    Functions that evaluate
+'''
+def dup(stack):
+    top = stack.pop()
+    stack.append(top)
+    stack.append(top)
+
+def swap(stack):
+    a = stack.pop()
+    b = stack.pop()
+    stack.append(a)
+    stack.append(b)
+
+def rot(stack):
+    a = stack.pop()
+    b = stack.pop()
+    c = stack.pop()
+    stack.append(c)
+    stack.append(a)
+    stack.append(b)
+
+def drop(stack):
+    stack.pop()
+
+def sum(stack):
+    summed = 0
+    while len(stack) != 0:
+        summed += stack.pop()
+    stack.append(summed)
+
+def avg(stack):
+    summed = 0
+    total = len(stack)
+    while len(stack) != 0:
+        summed += stack.pop()
+    stack.append(summed / total)
+
+def printtop(stack):
+    print(stack.pop())
+
+def add(stack):
+    a = stack.pop()
+    b = stack.pop()
+    stack.append(a + b)
+
+def subtract(stack):
+    a = stack.pop()
+    b = stack.pop()
+    stack.append(a - b)
+
+def multiply(stack):
+    a = stack.pop()
+    b = stack.pop()
+    stack.append(a * b)
+
+def divide(stack):
+    a = stack.pop()
+    b = stack.pop()
+    stack.append(a / b)
+
+'''
+    Map keywords to the evaluating functions
+'''
+KEYWORDS = {
+    "dup": dup,
+    "swap": swap,
+    "rot": rot,
+    "drop": drop,
+    "avg": avg,
+    "print": printtop,
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
--- main.py
+++ main.py
@@ -1,83 +1,8 @@
+from evaluate import KEYWORDS
+
 expression = "15.5 1.5 + 2 swap + 2 5 * - print "
 stack = []
 ptr = 0
-
-'''
-    Functions that evaluate
-'''
-def dup():
-    top = stack.pop()
-    stack.append(top)
-    stack.append(top)
-
-def swap():
-    a = stack.pop()
-    b = stack.pop()
-    stack.append(a)
-    stack.append(b)
-
-def rot():
-    a = stack.pop()
-    b = stack.pop()
-    c = stack.pop()
-    stack.append(c)
-    stack.append(a)
-    stack.append(b)
-
-def drop():
-    stack.pop()
-
-def sum():
-    summed = 0
-    while len(stack) != 0:
-        summed += stack.pop()
-    stack.append(summed)
-
-def avg():
-    summed = 0
-    total = len(stack)
-    while len(stack) != 0:
-        summed += stack.pop()
-    stack.append(summed / total)
-
-def printtop():
-    print(stack.pop())
-
-def add():
-    a = stack.pop()
-    b = stack.pop()
-    stack.append(a + b)
-
-def subtract():
-    a = stack.pop()
-    b = stack.pop()
-    stack.append(a - b)
-
-def multiply():
-    a = stack.pop()
-    b = stack.pop()
-    stack.append(a * b)
-
-def divide():
-    a = stack.pop()
-    b = stack.pop()
-    stack.append(a / b)
-
-'''
-    Map keywords to the evaluating functions
-'''
-KEYWORDS = {
-    "dup": dup,
-    "swap": swap,
-    "rot": rot,
-    "drop": drop,
-    "avg": avg,
-    "print": printtop,
-    "+": add,
-    "-": subtract,
-    "*": multiply,
-    "/": divide
-}
 
 '''
     Parsing Functions
@@ -123,7 +48,7 @@ if __name__ == "__main__":
         elif c.isalpha() or c in "+-/*":
             word = parseWord()
             if word in KEYWORDS:
-                KEYWORDS[word]()
+                KEYWORDS[word](stack)
             else:
                 print("Unknown word '" + word + "'. Exiting.")
                 exit()
